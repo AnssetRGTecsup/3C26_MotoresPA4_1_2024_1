728a8219fd103c98e965dbe63feea918693dd289
Sebastian Andre Salas Córdova <sebastian.salas@tecsup.edu.pe>
2024-06-18 09:51:20 -0500

    Video game screen flow

---
+++ Assets/Scripts/VictoryAndDefeatManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryAndDefeatManager : MonoBehaviour
+{
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+}
